6e055f14810f4007e6a4ddce7aa2f9e5d0bacfb4
koston022 <koston022@mail.ru>
2021-01-25 23:25:11 +0500

    lesson 012 Petrichenko

---
+++ app.js
@@ -0,0 +1,19 @@
+'use strict';
+let numberOfFilm = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let personalMovieDB = {
+    count: numberOfFilm,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+function askUser() {
+    for (let i = 0; i < 2; i++) {
+        let askMovie = prompt('Один из последних просмотренных фильмов?', '');
+        let askMovieRang = prompt('На сколько оцените его?', '');
+        personalMovieDB.movies[askMovie] = askMovieRang;
+    }
+}
+askUser()
+
+console.log(personalMovieDB)
